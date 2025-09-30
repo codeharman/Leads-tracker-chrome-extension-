4bcb9009327d1e15e9395459490f237518711227
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-09-30 21:09:42 +0530

    made the final edit

--- app.js
+++ app.js
@@ -9,6 +9,41 @@ const ulEl = document.getElementById('ul-el');
 let myLeads = [];
 
 // Need to work on the LocalStorage part
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render();
+}
+
 // Need to work on the rendering part
+function render() {
+  let listItems = '';
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li><a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a></li>`
+  }
+  ulEl.innerHTML = listItems;
+}
+
+// Need to work on the input button part
+inputBtn.addEventListener('click', function() {
+  myLeads.push(inputEl.value)
+  inputEl.value = '';
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
+  render();
+})
+
 // Need to work on the delete button part
+deleteBtn.addEventListener('click', function() {
+  localStorage.clear();
+  myLeads = [];
+  render();
+})
 // Need to work on the save tab part
+saveBtn.addEventListener('click', function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+    render();
+  });
+})
